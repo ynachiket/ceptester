cdaa6f66c2b4cdcaaefff8e06592e183292c66b2
Nachiket Torwekar <nachiket.torwekar@rackspace.com>
2014-06-13 12:21:00 -0700

    adding consecutive count ability

---
+++ ceptester/StateChange.java
@@ -0,0 +1,24 @@
+package ceptester;
+
+public class StateChange {
+	
+	String initialState;
+	String finalState;
+	
+	public String getInitialState() {
+		return initialState;
+	} 
+	
+	public String getFinalState() {
+		return finalState;
+	}
+	
+	public String setInitialState(String state) {
+		return initialState = state;
+	}
+	
+	public String setFinalState(String state) {
+		return finalState = state;
+	}
+
+}
