f4f1f60228ba0905b3b3a7fb0e279c856efc5d73
Phoebe Simon <phoebe.simon@rackspace.com>
2014-06-13 12:03:26 -0700

    Implemented consecutive count for all consistencylevels TODO: still need to figure out how to disregard observations considered 'too old'
    To use this:
    Drop SimulateState.java into java/tests/com/cloudkick/util
    Drop SimulateStateTest.java into java/tests/com/cloudkick/cep/test
    
    Changed from using Strings to represent states, to using AlarmStates
    Now allows you to examine the state at any point in the map, assuming that telescopes are delivered in the following order:
    1 4 7 10
    2 5 8 11
    3 6 9 12

--- ceptester/SimulateState.java
+++ ceptester/SimulateState.java
@@ -1,110 +1,204 @@
-package ceptester;
+package com.cloudkick.util;
+
+import telescope.thrift.AlarmState;
+
+import javax.xml.transform.OutputKeys;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class SimulateState {
 
-	HashMap<String, Integer> counterValues = new HashMap<String, Integer>();
-	
+    HashMap<String, Integer> counterValues = new HashMap<String, Integer>();
+
     public SimulateState() {
-    	
-		counterValues.put("OK", 0);
-		counterValues.put("Warning", 0);
-		counterValues.put("Critical", 0);
     }
-	// 1 OK , 2 Warn, 3 Critical
+    // 1 OK , 2 Warn, 6 Critical
 
-	/*
-	 * int observationsMap [][]= {{1, 1, 2, 2, 2}, {1, 1, 1, 1, 1}, {1, 1, 1, 2,
-	 * 2}, {1, 1, 1, 2, 2}};
-	 */
-	public String getState(int consecutiveCount, String consistencyLevel, int index,
-			int[][] observationsMap) {
+    /*
+      * int observationsMap [][]= {{1, 1, 2, 2, 2}, {1, 1, 1, 1, 1}, {1, 1, 1, 2,
+      * 2}, {1, 1, 1, 2, 2}};
+      */
+    public static AlarmState getState(int consecutiveCount, String consistencyLevel, int mz, int timePeriod,
+                           int[][] observationsMap) {
 
-		String state = null;
+        if (consistencyLevel.equalsIgnoreCase("One")) {
+            return  getStateForOne(getLatestObservationForOne(consecutiveCount, observationsMap, mz, timePeriod));
+        } else if (consistencyLevel.equalsIgnoreCase("All")) {
+            return getStateForAll(getLatestObservations(consecutiveCount, observationsMap, mz, timePeriod));
 
-		if (consecutiveCount > 1) {
-			return getStateForPluralConsecutiveCount(consecutiveCount, consistencyLevel,
-					index, observationsMap);
-		}
+        } else if (consistencyLevel.equalsIgnoreCase("Quorum")) {
+            return getStateForQuorum(getLatestObservations(consecutiveCount, observationsMap, mz, timePeriod));
+        }
 
-		if (consistencyLevel.equalsIgnoreCase("All")) {
-			state = getStateForAll(consecutiveCount, consistencyLevel, index,
-					observationsMap);
+        return null;
 
-		} else if (consistencyLevel.equalsIgnoreCase("Quorum")) {
-			state = getStateForQuorum(consecutiveCount, consistencyLevel, index,
-					observationsMap);
-
-		}
-
-		return state;
-
-	}
-
-	private String getStateForQuorum(int consecutiveCount, String method,
-			int index, int[][] observationsMap) {
-
-		int monitoringZones = observationsMap.length;
-		int quorumCount = monitoringZones / 2 + 1;
-		String state = null;
-
-		counterValues = getCounterValuesAtIndex(index, observationsMap);
-
-		for (Entry<String, Integer> entry : counterValues.entrySet()) {
-			if (entry.getValue() >= quorumCount) {
-				state = entry.getKey();
-			}
-		}
-
-		return state;
-	}
+    }
 
 
+    private static AlarmState getStateForQuorum(int[] latestObservations) {
+        int monitoringZones = latestObservations.length;
+        int quorumCount = monitoringZones / 2 + 1;
+        int ok = 0;
+        int crit = 0;
+        int warn = 0;
 
-	private String getStateForAll(int consecutiveCount, String method,
-			int index, int[][] observationsMap) {
-		
-		int monitoringZones = observationsMap.length;
-		String state = null;
+        for (int i = 0; i < latestObservations.length; i++) {
+            switch (latestObservations[i]) {
+                case 1:
+                    ok++;
+                    break;
+                case 2:
+                    warn++;
+                    break;
+                case 6:
+                    crit++;
+                    break;
+            }
 
-		counterValues = getCounterValuesAtIndex(index, observationsMap);
+        }
 
-		for (Entry<String, Integer> entry : counterValues.entrySet()) {
-			if (entry.getValue() == monitoringZones) {
-				state = entry.getKey();
-			}
-		}
+        if (crit >= quorumCount) {
+            return AlarmState.CRITICAL;
+        } else if (warn + crit >= quorumCount) {
+            return AlarmState.WARNING;
+        } else if (ok >= quorumCount) {
+            return AlarmState.OK;
+        }
 
-		return state;
-	}
-	
-	private HashMap<String, Integer> getCounterValuesAtIndex(int index,
-			int[][] observationsMap) {
-		
-		for (int j = 0; j < observationsMap.length; j++) {
+        return null;
+    }
 
-			if (observationsMap[j][index] == 1) {
-				counterValues.put("OK", counterValues.get("OK") + 1);
-			} else if (observationsMap[j][index] == 2) {
-				counterValues.put("Warning", counterValues.get("Warning") + 1);
-			} else if (observationsMap[j][index] == 3) {
-				counterValues
-						.put("Critical", counterValues.get("Critical") + 1);
-			}
-		}
-		return counterValues;
-	}
-	
 
-	private String getStateForPluralConsecutiveCount(int consecutiveCount,
-			String consistencyLevel, int index, int[][] observationsMap) {
-		//TODO
-		
 
-		return null;
-	}
+    private static AlarmState getStateForAll(int[] latestObservations) {
+
+        int monitoringZones = latestObservations.length;
+        int ok = 0;
+        int crit = 0;
+        int warn = 0;
+
+        for (int i = 0; i < latestObservations.length; i++) {
+            switch (latestObservations[i]) {
+                case 1:
+                    ok++;
+                    break;
+                case 2:
+                    warn++;
+                    break;
+                case 6:
+                    crit++;
+                    break;
+            }
+
+        }
+
+        if (crit == monitoringZones) {
+            return AlarmState.CRITICAL;
+        } else if (warn == monitoringZones) {
+            return AlarmState.WARNING;
+        } else if (ok == monitoringZones) {
+            return AlarmState.OK;
+        }
+
+        return null;
+    }
+
+
+    private static AlarmState getStateForOne(int latestObservation) {
+        switch (latestObservation) {
+            case 1:
+                return AlarmState.OK;
+            case 2:
+                return AlarmState.WARNING;
+            case 6:
+                return AlarmState.CRITICAL;
+            default:
+                return null;
+        }
+    }
+
+
+    public static int getLatestObservationForOne(int consecutiveCount, int[][] observations, int mz, int timePeriod) {
+        if (consecutiveCount == 1) {
+            return observations[mz][timePeriod];
+        } else {
+            int state = 0;
+            int countSoFar = 0;
+            for (int i = 0; i < consecutiveCount; i++) {
+                if (timePeriod - i < 0) {
+                    break;
+                }
+                if (observations[mz][timePeriod - i] == state) {
+                    countSoFar++;
+                } else {
+                    state = observations[mz][timePeriod - i];
+                    countSoFar = 1;
+                }
+            }
+
+            if (countSoFar == consecutiveCount) {
+                return state;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public static int[] getLatestObservations(int consecutiveCount, int[][] observations, int mz, int timePeriod) {
+        int[] latestObservations = new int[observations.length];
+        int _mz = mz;
+        int _time = timePeriod;
+
+        if (consecutiveCount == 1) {
+            for (int i = 0; i < observations.length; i++) {
+                if (_mz == 0 && _time == 0) {
+                    latestObservations[i] = observations[_mz][_time];
+                    break;
+                } else if (_mz == 0 && _time != 0) {
+                    latestObservations[i] = observations[_mz][_time];
+                    _mz = observations.length - 1;
+                    _time -= 1;
+                } else {
+                    latestObservations[i] = observations[_mz][_time];
+                    _mz -= 1;
+                }
+            }
+            return latestObservations;
+        } else {
+            int[][] latestConsecutiveObservations = new int[observations.length][2];
+            for (int i = 0; i < observations.length; i++) {
+                for (int j = _time; j >= 0; j--) {
+                    if (observations[_mz][j] == latestConsecutiveObservations[i][0]) {
+                        latestConsecutiveObservations[i][1]++;
+                    } else {
+                        latestConsecutiveObservations[i][0] = observations[_mz][j];
+                        latestConsecutiveObservations[i][1] = 1;
+                    }
+
+                    if (latestConsecutiveObservations[i][1] == consecutiveCount) {
+                        break;
+                    }
+                }
+
+                if (_mz == 0 && _time == 0) {
+                    break;
+                } else if (_mz == 0 && _time != 0) {
+                    _mz = observations.length - 1;
+                    _time -= 1;
+                } else {
+                    _mz -= 1;
+                }
+            }
+
+            for (int i = 0; i < latestConsecutiveObservations.length; i++) {
+                if (latestConsecutiveObservations[i][1] == consecutiveCount) {
+                    latestObservations[i] = latestConsecutiveObservations[i][0];
+                } else {
+                    latestObservations[i] = 0;
+                }
+            }
+            return latestObservations;
+        }
+    }
 
 }
--- ceptestertest/SimuateStateTest.java
+++ ceptestertest/SimuateStateTest.java
@@ -1,39 +1,174 @@
-package ceptestertest;
+package com.cloudkick.cep.test;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import ceptester.SimulateState;
+import com.cloudkick.util.SimulateState;
+import telescope.thrift.AlarmState;
 
-public class SimuateStateTest {
+import java.util.Arrays;
 
-	@Test
-	public void testALL() {
-		SimulateState simulatestate = new SimulateState(); 
-        String state;
+public class SimulateStateTest {
+
+    @Test
+    public void testGetLatestObservations() {
+        int[][] observations = {{1, 1, 2, 2, 6},
+                                {1, 1, 1, 1, 6},
+                                {1, 1, 1, 2, 6},
+                                {1, 1, 1, 2, 2}};
+
+        int[] latestObservations = SimulateState.getLatestObservations(1, observations, 3, 2);
+        assertTrue(Arrays.equals(latestObservations, new int[]{1, 1, 1, 2}));
+
+        latestObservations = SimulateState.getLatestObservations(1, observations, 2, 2);
+        assertTrue(Arrays.equals(latestObservations, new int[]{1, 1, 2, 1}));
+
+        latestObservations = SimulateState.getLatestObservations(1, observations, 2, 0);
+        assertTrue(Arrays.equals(latestObservations, new int[]{1, 1, 1, 0}));
+
+        latestObservations = SimulateState.getLatestObservations(1, observations, 0, 0);
+        assertTrue(Arrays.equals(latestObservations, new int[]{1, 0, 0, 0}));
+
+        latestObservations = SimulateState.getLatestObservations(2, observations, 3, 2);
+        assertTrue(Arrays.equals(latestObservations, new int[] {1, 1, 1, 1}));
+
+        latestObservations = SimulateState.getLatestObservations(2, observations, 3, 3);
+        assertTrue(Arrays.equals(latestObservations, new int[]{1, 1, 1, 2}));
+
+        latestObservations = SimulateState.getLatestObservations(2, observations, 3, 0);
+        assertTrue(Arrays.equals(latestObservations, new int[]{0, 0, 0, 0}));
+
+        latestObservations = SimulateState.getLatestObservations(2, observations, 2, 0);
+        assertTrue(Arrays.equals(latestObservations, new int[]{0, 0, 0, 0}));
+
+        latestObservations = SimulateState.getLatestObservations(2, observations, 2, 2);
+        assertTrue(Arrays.equals(latestObservations, new int[] {1, 1, 1, 1}));
+    }
+
+    @Test
+    public void testGetLatestObservation() {
+        int[][] observations = {{1, 1, 2, 2, 6},
+                                {1, 1, 1, 1, 6},
+                                {1, 1, 1, 2, 6},
+                                {1, 1, 1, 2, 2}};
+
+        int latestObservation = SimulateState.getLatestObservationForOne(1, observations, 2, 2);
+        assertEquals(latestObservation, 1);
+
+        latestObservation = SimulateState.getLatestObservationForOne(1, observations, 2, 3);
+        assertEquals(latestObservation, 2);
+
+        latestObservation = SimulateState.getLatestObservationForOne(2, observations, 2, 2);
+        assertEquals(latestObservation, 1);
+
+        latestObservation = SimulateState.getLatestObservationForOne(2, observations, 2, 3);
+        assertEquals(latestObservation, 0);
+    }
+
+    @Test
+    public void testALL() {
+        AlarmState state;
         int observationsMap [][] = {{1, 1, 2, 2, 2},
-        		                   {1, 1, 1, 1, 1},
-        		                   {1, 1, 1, 2, 2},
-        		                   {1, 1, 1, 2, 2}};
+                                    {1, 1, 1, 1, 2},
+                                    {1, 1, 1, 2, 1},
+                                    {1, 1, 1, 2, 2}};
 
-		state = simulatestate.getState(1, "ALL", 1, observationsMap);	
-		System.out.println(state);
-		assertEquals("OK", state);
-	}
-	
+        state = SimulateState.getState(1, "ALL", 3, 1, observationsMap);
+        assertEquals(AlarmState.OK, state);
 
-	@Test
-	public void testQuorum() {
-		SimulateState simulatestate = new SimulateState(); 
-        String state;
-        int observationsMap [][] = {{1, 1, 2, 2, 3},
-        		                   {1, 1, 1, 1, 3},
-        		                   {1, 1, 1, 2, 3},
-        		                   {1, 1, 1, 2, 2}};
+        state = SimulateState.getState(1, "ALL", 2, 1, observationsMap);
+        assertEquals(AlarmState.OK, state);
 
-		state = simulatestate.getState(1, "Quorum", 4, observationsMap);	
-		System.out.println(state);
-		assertEquals("Critical", state);
-	}
+        state = SimulateState.getState(1, "ALL", 3, 2, observationsMap);
+        assertEquals(null, state);
+
+        state = SimulateState.getState(1, "ALL", 1, 4, observationsMap);
+        assertEquals(AlarmState.WARNING, state);
+
+        state = SimulateState.getState(2, "ALL", 3, 1, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(2, "ALL", 0, 2, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(2, "ALL", 0, 3, observationsMap);
+        assertEquals(null, state);
+    }
+
+
+    @Test
+    public void testQuorum() {
+        AlarmState state;
+        int observationsMap [][] = {{1, 1, 2, 2, 6, 6},
+                                    {1, 1, 1, 1, 6, 6},
+                                    {1, 1, 1, 2, 6, 6},
+                                    {1, 1, 1, 2, 2, 6}};
+
+        state = SimulateState.getState(1, "Quorum", 3, 4, observationsMap);
+        assertEquals(AlarmState.CRITICAL, state);
+
+        state = SimulateState.getState(1, "Quorum", 3, 2, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(2, "Quorum", 3, 2, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(2, "Quorum", 3, 3, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(2, "Quorum", 3, 5, observationsMap);
+        assertEquals(AlarmState.CRITICAL, state);
+
+        state = SimulateState.getState(1, "Quorum", 1, 4, observationsMap);
+        assertEquals(AlarmState.WARNING, state);
+
+        int observationsMap2 [][]= {{1, 1, 2, 1, 6, 6},
+                                    {1, 1, 1, 1, 6, 6},
+                                    {1, 1, 1, 2, 6, 6},
+                                    {1, 1, 1, 2, 2, 6}};
+
+        state = SimulateState.getState(1, "Quorum", 3, 3, observationsMap2);
+        assertEquals(null, state);
+
+        /*int[][] observations6 = new int[][]{{6, 1, 6, 6, 1, 1},
+                                            {1, 6, 1, 6, 1, 1},
+                                            {6, 1, 6, 6, 1, 1},
+                                            {1, 6, 1, 6, 1, 1},
+                                            {6, 1, 6, 6, 1, 1}};
+        state = SimulateState.getState(2, "Quorum", 2, 3, observations6);
+        assertEquals(null, state);*/
+    }
+
+    @Test
+    public void testOne() {
+        AlarmState state;
+        int observationsMap [][] = {{1, 1, 2, 2, 6, 6},
+                                    {1, 1, 1, 1, 6, 6},
+                                    {1, 1, 1, 2, 6, 6},
+                                    {1, 1, 1, 2, 2, 6}};
+
+        state = SimulateState.getState(1, "One", 0, 0, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(1, "One", 0, 2, observationsMap);
+        assertEquals(AlarmState.WARNING, state);
+
+        state = SimulateState.getState(1, "One", 1, 4, observationsMap);
+        assertEquals(AlarmState.CRITICAL, state);
+
+        state = SimulateState.getState(2, "One", 1, 3, observationsMap);
+        assertEquals(AlarmState.OK, state);
+
+        state = SimulateState.getState(2, "One", 0, 2, observationsMap);
+        assertEquals(null, state);
+
+        state = SimulateState.getState(2, "One", 0, 3, observationsMap);
+        assertEquals(AlarmState.WARNING, state);
+
+        state = SimulateState.getState(3, "One", 1, 3, observationsMap);
+        assertEquals(AlarmState.OK, state);
+    }
+
+
 }
